c07b4d84705819037ee5976cccf152895e7243ba
gfuccio <g.fuccio359@gmail.com>
2018-06-12 19:39:19 +0200

    SentimentAnalyzer was cleaned and commented
    The class SentimentAnalyzer has been cleaned from unuseful logs message and commented

--- models/SentimentAnalyzer.py
+++ models/SentimentAnalyzer.py
@@ -1,43 +1,38 @@
 import json
 import urllib.parse,urllib.request
+from urllib.error import HTTPError
+
 
 class SentimentAnalyzer:
-
+    """
+    This class has the purpose to analyze a text and extract the sentiment from that text.
+    """
     def __init__(self):
         pass
 
 
     def extract_sentiment(self, text):
+        """
+        Given a text the sentiment is returned
+        :param text: text to analyze
+        :return: sentiment: Pos, Neutral or Neg
+        """
         sentiment = self.__get_sentiments(text)
         #Label key return us the sentiment with highest probability
         return sentiment['label']
 
     def __get_sentiments(self, text):
-        data = urllib.parse.urlencode({"text": text}).encode("utf-8")
-        u = urllib.request.urlopen("http://text-processing.com/api/sentiment/", data)
-        the_page = u.read()
-        toReturn = json.loads(the_page.decode())
-        return toReturn
-
-
-    # def getNegative(self, stringa):
-    #     print("STRINGA OTTENUTA: ", stringa)
-    #     dizionario = self.analyze_text(stringa)
-    #     return dizionario["probability"]["neg"]
-    #
-    # def getNeutral(self, stringa):
-    #     print("STRINGA OTTENUTA: ", stringa)
-    #     dizionario = self.analyze_text(stringa)
-    #     return dizionario["probability"]["neutral"]
-    #
-    # def getPositive(self, stringa):
-    #     print("STRINGA OTTENUTA: ", stringa)
-    #     dizionario = self.analyze_text(stringa)
-    #     return dizionario["probability"]["pos"]
-
-
-
-# if __name__ == '__main__':
-#     s = SentimentAnalyzer()
-#     print(s.get_label("You're a poor jerk!"))
-#     print(s.get_label("I love you!"))
+        """
+        Service request for sentiment analysis
+        :param text: text to analyze
+        :return: dictionary with sentiments as key and value its probability
+        """
+        try:
+            data = urllib.parse.urlencode({"text": text}).encode("utf-8")
+            u = urllib.request.urlopen("http://text-processing.com/api/sentiment/", data)
+            the_page = u.read()
+            toReturn = json.loads(the_page.decode())
+            return toReturn
+        except HTTPError:
+            toReturn = {'label': 'neutral'}
+            return toReturn
