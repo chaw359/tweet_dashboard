20a9ef2193f566a2e4bd954fba01dd92e8b0cc6f
gfuccio <g.fuccio359@gmail.com>
2018-06-12 21:55:24 +0200

    Update of the TopicExtractor class and comments has been added
    The TopicExtractor class has been updated fixing some bugs and has been commented

--- models/TopicExtractor.py
+++ models/TopicExtractor.py
@@ -4,15 +4,21 @@ import xml.etree.ElementTree as ET
 
 
 class TopicExtractor:
-
+    """
+    This class evaluates the topic of a given a text. In particular, for each tweet from the stream
+    this class evaluates the topic making requests to wikify to get the topic of tweet's text.
+    """
     def __init__(self):
-        #categorie da ricercare all'interno della gerarchia di wikipedia
+        #These are macro categories of Wikipedia that will be matched we the tweet topic
         self.categoryToFind = ["Technology", "Music", "Sport", "Finance", "Economy", "Politics",
                           "Entertainment", "Games", "Nutrition", "Medicine", "History", "Mathematics",
                           "Biology", "Nature", "Religion", "Society", "Business", "Computing",
                           "Electronics", "Engineering", "Transport"]
+        #To check if the topic is found
         self.found = False
-        #Indica le categorie trovate, si tratta di un dizionario che mantiene la frequenze delle categorie trovate
+
+        """This represent the categories found. In particular, it is a dictionary that holds the frequency of the 
+        category found"""
         self.categoryFound = {"Technology": 0, "Music": 0, "Sport": 0, "Finance":0, "Economy":0, "Politics":0,
                  "Entertainment":0, "Games":0, "Nutrition":0, "Medicine":0, "History":0, "Mathematics":0,
                  "Biology":0, "Nature":0, "Religion":0, "Society":0, "Business":0, "Computing":0,
@@ -25,7 +31,16 @@ class TopicExtractor:
         self.main_topics = {}
 
 
-    def check_parent_category(self, category_id, num_call):
+    def check_parent_category(self, category_id, num_call = 20):
+        """
+        This is a recursive function that navigates the category tree of wikipedia. To avoid infinite recursion process
+        we set a fixed number of call, if within these calls the category isn't found, we set the topic of the text as
+        Various. So, with the category id, wikify services returns the ids of parent categories that will be used to go
+        up the tree, but only the first branch of the tree.
+        :param category_id: the category id that represent the leaf to start the tree's climbing
+        :param num_call: maximumn number of call to avoid infinite recursion process
+        :return: the label of the category found
+        """
         url = "http://193.205.163.45:8080/wikipedia-miner/services/exploreCategory?id=" + str(category_id) + "&responseFormat=json&parentCategories=true"
         categoryResponse = requests.post(url)
         categories = categoryResponse.json()
@@ -35,7 +50,7 @@ class TopicExtractor:
             return "Various"
 
         if categories['title'] in self.categoryToFind:
-            print(categories['title'] + " found!")
+            #print(categories['title'] + " found!")
             #categoryFound[categories['title']] = 1
             return categories['title']
         else:
@@ -49,31 +64,40 @@ class TopicExtractor:
 
 
     def analyze_text(self, text):
+        """
+        This method analyzes the given text and returns the best category found for the text.
+        :param text: text to analyze
+        :return: the best category found, that match with the category to find
+        """
         self.main_topics = {}
         self.best_categories = []
+        #with this call we found the first level of categories detected for the given text
         urlSearch = "http://193.205.163.45:8080/wikipedia-miner/services/wikify?source=" + text + "&responseFormat=json"
         response = requests.post(urlSearch)
         try:
             topics = response.json()['detectedTopics']
             #print(topics)
+            #this method's call start the recursion if the first level of topics isn't in the categoryToFind list
             self.__wikify_request_for_categories(topics)
 
             print("Main topic:")
+            #each category that overcome the zero is a main topic of the given text
             for key in self.categoryFound:
                 if self.categoryFound[key] > 0:
                     print(key, ": ", self.categoryFound[key])
                     self.main_topics[key] = self.categoryFound[key]
-        except JSONDecodeError:# Per alcuni testi non viene fornita la risposta JSON quindi si fa il parsing dell' XML
+        except JSONDecodeError:# For some texts isn't returned the json, so we make the parsing of the returned XML
             print("No Json... XML Parsing...")
-            #attenzione ai detected topics che potrebbero non esserci
+
             root = ET.fromstring(response.text)
             topics =[]
             for child in root:
-                if child.tag == "detectedTopics":
+                if child.tag == "detectedTopics":#detectedTopics is the attribute of our interest
                     for topicChild in child:
                         topics.append({"id": topicChild.attrib['id'], "title": topicChild.attrib['title'], "weight":topicChild.attrib["weight"]})
                         #print(topicChild.tag, topicChild.attrib['id'])
             #print(topics)
+
             self.__wikify_request_for_categories(topics)
 
             print("Main topic:")
@@ -81,33 +105,49 @@ class TopicExtractor:
                 if self.categoryFound[key] > 0:
                     print(key, ": ", self.categoryFound[key])
                     self.main_topics[key] = self.categoryFound[key]
+        except KeyError:
+            pass
 
 
         self.__evaluate_best_topics()
-
-        return self.best_categories
+        if len(self.best_categories) == 0:
+            return "Various"
+        else:
+            return self.best_categories[0]
 
 
 
     def __evaluate_best_topics(self):
+        """
+        This method is used to find the best categories. The best categories are the categories that in frequency
+        overcome the average
+        :return: None
+        """
         summation = 0
-        if len(self.main_topics.keys()) == 1:
-            self.best_categories.append(list(self.main_topics.keys())[0])
+        if len(self.main_topics.keys()) > 0:
+            if len(self.main_topics.keys()) == 1:
+                self.best_categories.append(list(self.main_topics.keys())[0])
 
-        for key in self.main_topics:
-            summation += self.main_topics[key]
+            for key in self.main_topics:
+                summation += self.main_topics[key]
 
-        avg = summation/len(self.main_topics.keys())
-        print("Average:", avg)
-        for key in self.main_topics:
-            if self.main_topics[key] > avg:
-                self.best_categories.append(key)
+            avg = summation/len(self.main_topics.keys())
+            print("Average:", avg)
+            for key in self.main_topics:
+                if self.main_topics[key] >= avg:
+                    self.best_categories.append(key)
 
-        print("The best categories are:")
-        print(self.best_categories)
+            print("The best categories are:")
+            print(self.best_categories)
 
     def __wikify_request_for_categories(self, topics):
-
+        """
+        For each topic in the list of topics passed a call to wikify service is done to get the first level of parent
+        categories of the current topic. If one of these parent categories matches with categoryToFind list, its counter
+        is updated, otherwise for each category in the parent categories a call to recursive function is done.
+        :param topics: list of topic as dictionary
+        :return: None
+        """
         topicFound = False
         for topic in topics:
             #print(topic)
@@ -118,14 +158,17 @@ class TopicExtractor:
             response = requests.post(urlArticle)
             categories = response.json()
             #print(response.json())
-            if (categories['title'] in self.categoryToFind):
-                self.categoryFound[categories['title']] += 1
-                break
-            else:
-                for category in categories['parentCategories']:
-                    found = self.check_parent_category(category['id'], 20)
-                    if found in self.categoryToFind:
-                        self.categoryFound[found] += 1
-                        topicFound = True
-                if topicFound:
+            try:
+                if (categories['title'] in self.categoryToFind):
+                    self.categoryFound[categories['title']] += 1
                     break
+                else:
+                    for category in categories['parentCategories']:
+                        found = self.check_parent_category(category['id'], 20)
+                        if found in self.categoryToFind:
+                            self.categoryFound[found] += 1
+                            topicFound = True
+                    if topicFound:
+                        break
+            except KeyError:
+                continue
