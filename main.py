d2b563fa22586859968a579820e375760ebfa3b4
gfuccio <g.fuccio359@gmail.com>
2018-06-12 23:18:43 +0200

    main file is updated

--- main.py
+++ main.py
@@ -1,47 +1,16 @@
-import re
-import sys
-
+#!/usr/bin/env python
 from models.TweetScraper import TweetScraper
-import pandas as pd
-from subprocess import call
 import os
+import sys
+#if you want run the project from here decomment this line
+#userToMonitor = "realDonaldTrump"
 
+#if you want to run from command shell and from the dashboard
 userToMonitor = sys.argv[1]
-scraper = TweetScraper()
-scraper.tweet_query(userToMonitor, begin_year=2017)
 
+deletecommand = 'curl -X DELETE "localhost:9200/tweet_dataset/"'
 
-
-#data = pd.read_csv("dataset/tweet_dataset.csv", sep=",")
-
-# print(data)
-#
-# curl_list = []
-# for index, row in data.iterrows():
-#     document = '\'{"tweet_id":"' + str(row['tweet_text']) + '", "Topic":"' + str(row['username']) + '"}\''
-#     command_string = ["curl", "-X", "PUT",'\"localhost:9200/tweet_dataset/tweet/' + str(row['tweet_id']) +'\"', "-H", "'Content-Type: application/json'",
-#                   "-d", document]
-#
-#     curlstring = 'curl -X PUT "localhost:9200/tweet_dataset/tweet/' + str(row['tweet_id']) + '" -H ' + "'Content-Type: application/json' -d "
-#
-#     print(curlstring + document)
-#     curl_list.append(curlstring + document)
-
-#     for arg in command_string:
-#         print(arg, end=" ")
-#
-#     curl_list.append(command_string)
-#     print("\n")
-#
-# call(curl_list[2])
-
-#os.system(curl_list[2])
-
-# print("First curl request:")
-# print(curl_list[0])
-# call(curl_list[0])
-
-
-
-
+if not os.system(deletecommand):
+    scraper = TweetScraper()
+    scraper.tweet_query(userToMonitor, begin_year=2015)
 
