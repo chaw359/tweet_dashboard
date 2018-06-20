8370825f78c9591a61e77e8d4d4dc4f13237ebe7
gfuccio <g.fuccio359@gmail.com>
2018-06-20 10:01:29 +0200

    RelationsExtractor has been commented and cleaned
    The class RelationsExtractor has been commented and cleaned removing unuseful log messages and print

--- models/RelationsExtractor.py
+++ models/RelationsExtractor.py
@@ -1,40 +1,63 @@
 import csv
 import json
 import random as rand
+import re
+
+import pandas as pd
+from ast import literal_eval
 from collections import OrderedDict
 
 maxIDValue = 999999999999
 maxLinks = 5
-class RelationsExtractor:
 
-    def __init__(self, user = "Giovanni"):
+
+class RelationsExtractor:
+    """
+    This class has the purpose to extracts relations from the dataset and for each relation is assigned
+    the sentiment.
+    """
+    def __init__(self, user = "Dummy"):
         self.user = user
         self.linkSentiment = dict()
         self.linkIDs = dict()
         self.linkIDs[user] = rand.randrange(maxIDValue)
 
-    def getRelations(self, fileToRead = "resources/tweets.csv"):
-        with open(fileToRead, 'r') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            ls = dict()
-            for row in reader:
-                sentiment = row[4]
-                links = row[5].split()
+    def read_links(self, from_file = "../relation_dataset/tweet_dataset.csv"):
+        """
+        This method reads from the dataset the column link that represents the link
+        between the monitored user and other twitter's users. After this call a json
+        file is created.
+        :param from_file: path where is positioned the dataset to read
+        :return: after the call a json file in the same folder of the dataset is created
+        """
+        data = pd.read_csv(from_file, sep=";", index_col=0, quotechar='"')
+        for index, row in data.iterrows():
+            links = literal_eval(row['link'])
+            sentiment = row['sentiment']
+            if len(links) > 0:
                 for link in links:
+
+                    link = " ".join(re.findall("[a-zA-Z]+", link))
                     if link not in self.linkSentiment:
                         self.linkSentiment[link] = dict()
                         self.linkSentiment[link]["total"] = 0
                         self.linkIDs[link] = rand.randrange(maxIDValue)
-                        
+
                     if sentiment not in self.linkSentiment[link]:
                         self.linkSentiment[link][sentiment] = 0
-                        
+
                     self.linkSentiment[link][sentiment] += 1
                     self.linkSentiment[link]["total"] += 1
-                        
-        self.sendRelationsJSON()
 
-    def sendRelationsJSON(self):
+        #print(self.linkSentiment)
+        self.__create_graph_file()
+
+    def __create_graph_file(self):
+        """
+        This method takes the list created in the read_links method and creates the json
+        structure that represents the connected graph.
+        :return: a json file is created
+        """
         
         idUser = str(self.linkIDs[self.user])
         jsonString='{ "nodes": [{"name":"'+self.user+'","label":"'+self.user+'","id":'+idUser+'},'
@@ -43,12 +66,18 @@ class RelationsExtractor:
         for link,sentiments in linkSentiment:
             idLink = str(self.linkIDs[link])
             totalCit = int(sentiments["total"])
+            tmpList = {}
             for sent, freq in sentiments.items():
-                sentiments[sent] = str(int(freq*100 / totalCit))+"%"
-            del sentiments["total"]
+                if sent == "total":
+                    tmpList["numTags"] = str(freq)
+                else:
+                    tmpList[sent] = str(int(freq*100 / totalCit))+"%"
+                sentiments['total'] = 1
             jsonString += '{"name":"'+link+'","label":"'+link+'","id":'+idLink+'},'
-            jsonLinks += '{"source":'+idUser+',"target":'+idLink+',"type":"'+repr(sentiments).replace("'","")+'"},'
+            jsonLinks += '{"source":'+idUser+',"target":'+idLink+',"type":"'+repr(tmpList).replace("'","")+'"},'
         jsonString = jsonString[:-1]+'],"links":['+jsonLinks[:-1]+'] }'
-        
-        with open('resources/relations.json', 'w') as outfile:
+        #print(jsonString)
+
+        # here is needed the absolute path if we need to run the project via Web
+        with open('/Users/gfuccio/GitHub/tweet_dashboard/relation_dataset/relations.json', 'w') as outfile:
             outfile.write(jsonString)
