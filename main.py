8f6b57879b92b3534d39def0d72e8fbfaa0612d2
gfuccio <g.fuccio359@gmail.com>
2018-06-20 10:20:36 +0200

    Other comments has been added

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 from models.TweetScraper import TweetScraper
 import os
 import sys
-#if you want run the project from here decomment this line
-#userToMonitor = "realDonaldTrump"
+#if you want run the project from here and you won't use elasticsearch-kibana decomment till line 8 and comment from 11 to 16
+#userToMonitor = "Ibra_official"
+# scraper = TweetScraper()
+# scraper.tweet_query(userToMonitor, begin_year=2015)
 
-#if you want to run from command shell and from the dashboard
+
+#if you want to run from command shell and/or from the dashboard and you have elasticsearch activated decomment till line16
 userToMonitor = sys.argv[1]
-
 deletecommand = 'curl -X DELETE "localhost:9200/tweet_dataset/"'
-
 if not os.system(deletecommand):
-    scraper = TweetScraper()
-    scraper.tweet_query(userToMonitor, begin_year=2015)
-
+     scraper = TweetScraper()
+     scraper.tweet_query(userToMonitor, begin_year=2015)
