6d8663c2fdfe9c3e5815ad72a87089b7bfc2cd26
gfuccio <g.fuccio359@gmail.com>
2018-06-20 10:34:37 +0200

    Upload of web dashboard

--- web_dashboard/runScraper.php
+++ web_dashboard/runScraper.php
@@ -1,7 +1,9 @@
 <?php
-
+session_start();
 $command = escapeshellcmd('/Library/Frameworks/Python.framework/Versions/3.6/bin/python3 /Users/gfuccio/GitHub/tweet_dashboard/main.py '.$_POST["user"]);
 shell_exec($command);
+$_SESSION['username'] = $_POST['user'];
+echo "Redirecting to dashboard...";
 
-header("location: http://localhost/web_dashboard/dashboard.html");
+header('location:http://localhost/web_dashboard/dashboard.html');
 ?>
